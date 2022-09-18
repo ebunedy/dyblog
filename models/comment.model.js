1d549638ac2f5991efe52f76c06bf24bc9d5a6c4
Ebune Daniel Yahaya <ebunedy@gmail.com>
2022-09-18 14:04:34 +0100

    added avaerage rating to post

--- models/comment.model.js
+++ models/comment.model.js
@@ -30,4 +30,32 @@ const commentSchema = Schema(
   { timestamps: true }
 );
 
+commentSchema.index({ user: 1, post: 1 }, { unique: true });
+
+commentSchema.statics.averageRating = async function (postId) {
+  const averageRating = await this.aggregate([
+    { $match: { post: postId } },
+    { $group: { _id: null, averageRating: { $avg: "$rating" } } },
+  ]);
+
+  try {
+    await this.model("Post").findOneAndUpdate(
+      { _id: postId },
+      {
+        averageRating: Math.ceil(averageRating[0]?.averageRating || 0),
+      }
+    );
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+commentSchema.post("save", async function () {
+  await this.constructor.averageRating(this.post);
+});
+
+commentSchema.post("remove", async function () {
+  await this.constructor.averageRating(this.post);
+});
+
 module.exports = model("Comment", commentSchema);
